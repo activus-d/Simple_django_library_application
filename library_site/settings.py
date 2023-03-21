9af1a5785fa79fc7c21e6f93dd5435077d97854a
Storm Berê Heg <storm@stormheg.co>
2023-03-21 10:02:51 +0100

    Fix static files

--- library_site/settings.py
+++ library_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-une-#cc0ch@b)3^lzjpvrvw@ron=mw06p=1wf%d=^fjy7r6os8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
